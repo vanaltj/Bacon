a164bb511e2b042680ba900bd65f378cf7e682d6
Jon VanAlten <vanaltj@gmail.com>
2011-06-10 16:54:43 -0400

    Make it all nicer and better before adding anything new.  - Basically just found my head and added 10s of silence to the .ogg of the    alert to let MediaPlayer loop rather than convolutedly having gaps.

--- src/org/rhok/bacon/android/AlertNeedHelp.java
+++ src/org/rhok/bacon/android/AlertNeedHelp.java
@@ -1,40 +1,47 @@
 package org.rhok.bacon.android;
 
-import org.rhok.bacon.android.audio.AudibleAlertManager;
+import java.io.IOException;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ToggleButton;
 
 public class AlertNeedHelp extends Activity implements OnClickListener {
-	private Thread aat = null;
-	private AudibleAlertManager aam = null;
+	
 	private ToggleButton aac = null;
+	private MediaPlayer mp;
+	boolean active;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        active = false;
         setContentView(R.layout.main);
         aac = (ToggleButton) findViewById(R.id.togglebutton);
         aac.setOnClickListener(this);
-        //MediaPlayer mp = MediaPlayer.create(this, R.raw.emergency_alert_system_attention_signal30s);
-        //mp.start();
+        mp = MediaPlayer.create(this, R.raw.emergency_alert_system_attention_signal40s_loop);
+		mp.setLooping(true);
     }
 	@Override
 	public void onClick(View v) {
-		if (v instanceof ToggleButton) {
+		if (v.equals(aac)) { // Audible Alert toggle control.
 			if (((ToggleButton) v).isChecked()) {
-				aam = new AudibleAlertManager(getBaseContext(), R.raw.emergency_alert_system_attention_signal30s);
-				aat = new Thread(aam);
-				aat.start();
+				this.mp.start();
 			} else {
-				if (aat != null) {
-					aam.stop();
-					aat.interrupt();
-					aat = null;
+				active = false;
+				this.mp.stop();
+				try {
+					this.mp.prepare();
+				} catch (IllegalStateException ex) {
+					// TODO Auto-generated catch block
+					ex.printStackTrace();
+				} catch (IOException ex) {
+					// TODO Auto-generated catch block
+					ex.printStackTrace();
 				}
 			}
 		}
